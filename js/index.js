ea4b8904fd8a6edcafe0aa662006b381028d7e91
blurleisuree <ipishir@gmail.com>
2023-01-22 01:02:44 +0300

    change base slider structure

--- js/index.js
+++ js/index.js
@@ -8,8 +8,11 @@ $(document).ready(function () {
         sliderTransition = $(slider).css("transition"),
         width = slider.width();
 
-    let slideNow = 1,
-        widthNow = width - width,
+    // let slideNow = 1,
+    //     widthNow = width - width,
+    //     i = 1;
+
+    let slideIndex = 0,
         i = 1;
 
     let timeDelay = sliderTransition.match(/(\d+|0\.\d+)s/)[0];
@@ -28,59 +31,89 @@ $(document).ready(function () {
 
     // Next & prev -------------------------------------------------------------------------------
 
-    function nextSlide() {
-        if (slideNow == slideCount || slideNow > slideCount) {
-            // indicatorEmpty();
+    // function nextSlide() {
+    //     if (slideNow == slideCount || slideNow > slideCount) {
+    //         // indicatorEmpty();
 
-            // $(next).css("pointer-events", "none")
-            // setTimeout(function () {
-            //     $(next).css("pointer-events", "auto")
-            // }, timeDelay + 20)
+    //         // $(next).css("pointer-events", "none")
+    //         // setTimeout(function () {
+    //         //     $(next).css("pointer-events", "auto")
+    //         // }, timeDelay + 20)
 
-            // slider.css("transform", "translateX(0px)");
-            // slideNow = 1;
-            // widthNow = 0;
+    //         // slider.css("transform", "translateX(0px)");
+    //         // slideNow = 1;
+    //         // widthNow = 0;
 
-            // indicatorFull();
-            
+    //         // indicatorFull();
+
+    //         return
+    //     } else {
+    //         indicatorEmpty();
+
+    //         $(next).css("pointer-events", "none")
+    //         setTimeout(function () {
+    //             $(next).css("pointer-events", "auto")
+    //         }, timeDelay + 20)
+
+    //         widthNow -= width;
+    //         slider.css("transform", `translateX(${widthNow}px)`);
+    //         slideNow++;
+
+    //         indicatorFull();
+    //     };
+    // };
+
+    // function prevSlide() {
+    //     if (slideNow == 1) {
+    //         return
+    //     } else {
+    //         indicatorEmpty();
+
+    //         $(prev).css("pointer-events", "none")
+    //         setTimeout(function () {
+    //             $(prev).css("pointer-events", "auto")
+    //         }, timeDelay + 20)
+
+    //         widthNow += width;
+    //         slider.css("transform", `translateX(${widthNow}px)`);
+    //         slideNow--;
+
+    //         indicatorFull();
+    //     };
+    // };
+
+    // prev.click(prevSlide);
+    // next.click(nextSlide);
+
+    let slide = () => {
+        slider.css("transition", "all 0.5s ease-out");
+        slider.css("transform", `translateX(-${slideIndex * width}px)`);
+    }
+
+    prev.click(function () {
+        if (slideIndex == 0) {
             return
-        } else {
-            indicatorEmpty();
+        }
+        $(prev).css("pointer-events", "none")
+        setTimeout(function () {
+            $(prev).css("pointer-events", "auto")
+        }, timeDelay + 20)
 
-            $(next).css("pointer-events", "none")
-            setTimeout(function () {
-                $(next).css("pointer-events", "auto")
-            }, timeDelay + 20)
-
-            widthNow -= width;
-            slider.css("transform", `translateX(${widthNow}px)`);
-            slideNow++;
-
-            indicatorFull();
-        };
-    };
-
-    function prevSlide() {
-        if (slideNow == 1) {
+        slideIndex--;
+        slide();
+    })
+    next.click(function () {
+        if (slideIndex == sliderList.length - 1) {
             return
-        } else {
-            indicatorEmpty();
+        }
+        $(next).css("pointer-events", "none")
+        setTimeout(function () {
+            $(next).css("pointer-events", "auto")
+        }, timeDelay + 20);
 
-            $(prev).css("pointer-events", "none")
-            setTimeout(function () {
-                $(prev).css("pointer-events", "auto")
-            }, timeDelay + 20)
-
-            widthNow += width;
-            slider.css("transform", `translateX(${widthNow}px)`);
-            slideNow--;
-
-            indicatorFull();
-        };
-    };
-
-    prev.click(prevSlide);
-    next.click(nextSlide);
+        slideIndex++;
+        slide();
+    })
 
     // AutoSlider ----------------------------------------------------------------------------------
 
@@ -104,35 +137,43 @@ $(document).ready(function () {
 
     // Indicators ------------------------------------------------------------
 
-    let indicatorBackList = $('.slider__indicator_back');
-    let indicatorList = $('.slider__indicator');
-    indicatorFull();
+    // let indicatorBackList = $('.slider__indicator_back');
+    // let indicatorList = $('.slider__indicator');
+    // indicatorFull();
 
-    indicatorList.click(function () {
-        let indicatorIndex = $(this).index() + 1;
-        if (indicatorIndex == slideNow) {
-            return;
-        } else {
-            indicatorEmpty();
+    // indicatorList.click(function () {
+    //     let indicatorIndex = $(this).index() + 1;
+    //     if (indicatorIndex == slideNow) {
+    //         return;
+    //     } else {
+    //         indicatorEmpty();
 
-            widthNow += (slideNow - indicatorIndex) * width;
-            slider.css("transform", `translateX(${widthNow}px)`);
-            slideNow = indicatorIndex;
+    //         widthNow += (slideNow - indicatorIndex) * width;
+    //         slider.css("transform", `translateX(${widthNow}px)`);
+    //         slideNow = indicatorIndex;
 
-            indicatorFull();
-        };
-    });
+    //         indicatorFull();
+    //     };
+    // });
 
-    function indicatorFull() {
-        $(indicatorBackList[slideNow - 1]).show();
-        $(indicatorBackList[slideNow - 1]).css({ "width": "100%" });
-    };
+    // function indicatorFull() {
+    //     $(indicatorBackList[slideNow - 1]).show();
+    //     $(indicatorBackList[slideNow - 1]).css({ "width": "100%" });
+    // };
 
-    function indicatorEmpty() {
-        $(indicatorBackList[slideNow - 1]).hide();
-        $(indicatorBackList[slideNow - 1]).css({ "width": "0%" });
+    // function indicatorEmpty() {
+    //     $(indicatorBackList[slideNow - 1]).hide();
+    //     $(indicatorBackList[slideNow - 1]).css({ "width": "0%" });
 
-        $(indicatorBackList[slideNow - 1]).css('transition', `all ${indicatorBackTransition}s linear`)
-    }
-    // -------------------------------------------------------------------------
+    //     $(indicatorBackList[slideNow - 1]).css('transition', `all ${indicatorBackTransition}s linear`)
+    // }
+    // Touch -------------------------------------------------------------------------
+
+    // let posX1 = 0,
+    // posX2 = 0,
+    // posFinal = 0,
+    // posInit = 0,
+    // posThreshold = width * .35,
+    // trfRegExp = /[-0-9.]+(?=px)/;
+
 }); 
